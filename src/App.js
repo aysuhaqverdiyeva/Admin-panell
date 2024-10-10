724ac5eb9585c9fce787d3543e88f0eb80f359df
huseynmamedov <mamedovhuseyn616@gmail.com>
2024-10-10 11:57:25 +0400

    UPDATED

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import Sidebar from "./components/sidebar/Sidebar";
-import Users from "./components/pages/users/Users";
-import Application from "./components/pages/application/Application"
 import "./app.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Forgot from "./pages/forgot/Forgot";
 
 function App() {
   return (
